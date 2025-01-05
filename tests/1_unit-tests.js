7c0fdfcdaca9fbe3653b68f85d784938748cb795
doasmike <doasmike@gmail.com>
2025-01-05 23:24:51 +0000

    Added tests structure / WiP

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,52 @@ const Solver = require('../controllers/sudoku-solver.js');
 let solver;
 
 suite('Unit Tests', () => {
+    // Logic handles a valid puzzle string of 81 characters
+    test('Logic handles a valid puzzle string of 81 characters', () => {
 
+    });
+    // Logic handles a puzzle string with invalid characters (not 1-9 or .)
+    test('Logic handles a puzzle string with invalid characters (not 1-9 or .)', () => {
+
+    })
+    // Logic handles a puzzle string that is not 81 characters in length
+    test('Logic handles a puzzle string that is not 81 characters in length', () => {
+
+    })
+    // Logic handles a valid row placement
+    test('Logic handles a valid row placement', () => {
+
+    })
+    // Logic handles an invalid row placement
+    test('Logic handles an invalid row placement', () => {
+
+    })
+    // Logic handles a valid column placement
+    test('Logic handles a valid column placement', () => {
+
+    })
+    // Logic handles an invalid column placement
+    test('Logic handles an invalid column placement', () => {
+
+    })
+    // Logic handles a valid region (3x3 grid) placement
+    test('Logic handles a valid region (3x3 grid) placement', () => {
+
+    })
+    // Logic handles an invalid region (3x3 grid) placement
+    test('Logic handles an invalid region (3x3 grid) placement', () => {
+
+    })
+    // Valid puzzle strings pass the solver
+    test('Valid puzzle strings pass the solver', () => {
+
+    })
+    // Invalid puzzle strings fail the solver
+    test('Invalid puzzle strings fail the solver', () => {
+
+    })
+    // Solver returns the expected solution for an incomplete puzzle
+    test('Solver returns the expected solution for an incomplete puzzle', () => {
+
+    })
 });
